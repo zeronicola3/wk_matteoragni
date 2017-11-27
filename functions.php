3b02096484418f6f4725d0cc7af76dd2cc25d8be
Nicola Carpene <zeronicola3@gmail.com>
2017-11-27 17:31:51 +0100

    [mac2]

--- functions.php
+++ functions.php
@@ -470,7 +470,7 @@ function webkolm_gallery_meta_box( $object, $box ) {
     ?>
 
   <p>
-    <?php wp_editor( htmlspecialchars_decode($content), $editor_id, $settings ); ?>
+    <?php wp_editor( htmlspecialchars_decode($content), $editor_id); ?>
   </p>
 <?php }
 
