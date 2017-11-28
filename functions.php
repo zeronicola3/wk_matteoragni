7c4c80598656b62e327eaa722fa00cee0ddc967c
Nicola Carpene <zeronicola3@gmail.com>
2017-11-28 15:03:47 +0100

    [mac2]

--- functions.php
+++ functions.php
@@ -511,7 +511,7 @@ function webkolm_post_meta_boxes_setup() {
 function webkolm_save_metas($post_id, $post) {
 
 
-    $metas = array('webkolm_project_year','webkolm_prizes', 'webkolm_designer', 'webkolm_client_link');
+    $metas = array('webkolm_project_year','webkolm_prizes', 'webkolm_designer', 'webkolm_client_link', 'webkolm_gallery');
 
     // Get the post type object. 
     $post_type = get_post_type_object( $post->post_type );
