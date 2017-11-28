dc492c38ad659f389d340c6bed4a38bb5e668590
Nicola Carpene <zeronicola3@gmail.com>
2017-11-28 16:04:07 +0100

    [mac2]

--- index.php
+++ index.php
@@ -38,11 +38,15 @@ get_header(); ?>
                                 
                                 foreach ($array_id as &$item) {
                                     $url_small = wp_get_attachment_image_src( $item, 'medium' );
+                                    $url_small = wp_get_attachment_image_src( $item, 'medium' );
                                       
                                     ?>
                                     <li class="project_slide-<?= $numslide; ?> slideimg">
                                         <style>
-                                          .project-cover-gallery-<?php echo $elem_number; ?> .project_slide-<?= $numslide; ?> { background-image:url('<?php echo $url_small['0'] ?>');}
+                                          .project-cover-gallery-<?php echo $elem_number; ?> .project_slide-<?= $numslide; ?> { background-image:url('<?php echo $url_big['0'] ?>');}
+
+                                          @media (min-width: 768px) {  .project-cover-gallery-<?php echo $elem_number; ?> .project_slide-<?= $numslide; ?> { background-image:url('<?php echo $url_small['0'] ?>');}
+
                                         </style>
                                     </li>
                                     <?php $numslide++;
