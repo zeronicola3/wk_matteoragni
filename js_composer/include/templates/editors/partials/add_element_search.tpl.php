98f5c7556fba40be989f9ac3da4bb4cfe439a381
Nicola Carpene <zeronicola3@gmail.com>
2017-12-01 09:48:44 +0100

    [mac2]

--- js_composer/include/templates/editors/partials/add_element_search.tpl.php
+++ js_composer/include/templates/editors/partials/add_element_search.tpl.php
@@ -7,9 +7,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="vc_ui-panel-header-actions">
 	<div class="vc_ui-search-box">
 		<div class="vc_ui-search-box-input">
-			<input type="search" id="vc_templates_name_filter" data-vc-templates-name-filter
-			       placeholder="<?php echo __( 'Search template by name', 'js_composer' ) ?>">
-			<label for="vc_templates_name_filter">
+			<input type="search" id="vc_elements_name_filter"
+			       placeholder="<?php echo __( 'Search element by name', 'js_composer' ) ?>">
+			<label for="vc_elements_name_filter">
 				<i class="vc-composer-icon vc-c-icon-search"></i>
 			</label>
 		</div>
