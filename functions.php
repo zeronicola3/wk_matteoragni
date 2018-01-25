5c02ef213cda8d837dd1b9acab99592cde8a27b2
Nicola Carpene <zeronicola3@gmail.com>
2018-01-25 15:58:59 +0100

    [mac2]

--- functions.php
+++ functions.php
@@ -634,11 +634,11 @@ function be_attachment_field_credit( $form_fields, $post ) {
     $checked = ($ischeck) ? 'checked' : '';
 
     $form_fields['image-bg-size'] = array(
-        'label' => 'Fullwidth image:',
+        'label' => 'Immagine non tagliata:',
         'input' => 'html',
         'html' => "<input type='checkbox' {$checked} name='attachments[{$post->ID}][image-bg-size]' id='attachments[{$post->ID}][image-bg-size]' />",
         'value' => get_post_meta( $post->ID, 'image-bg-size', true ),
-        'helps' => 'If it is checked, that image take ',
+        'helps' => 'Se selezionato, l`immagine verrà visualizzata per intero',
     );
     
     return $form_fields;
