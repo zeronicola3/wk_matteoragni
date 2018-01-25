5987a8eb991a5ec02c8e7c939472bd81e804918e
Nicola Carpene <zeronicola3@gmail.com>
2018-01-25 23:32:31 +0100

    [mac2]

--- functions.php
+++ functions.php
@@ -607,7 +607,7 @@ function webkolm_featured_img_box( $object, $box ) { ?>
 
           // Set all variables to be used in scope
           var frame,
-              metaBox = $('#webkolm_featured_img.postbox'), // Your meta box id here
+              metaBox = $('#webkolm_featured_img'), // Your meta box id here
               addImgLink = metaBox.find('.upload-custom-img'),
               delImgLink = metaBox.find( '.delete-custom-img'),
               imgContainer = metaBox.find( '.custom-img-container'),
@@ -647,6 +647,8 @@ function webkolm_featured_img_box( $object, $box ) { ?>
               imgIdInput.val( attachment.id );
 
               console.log(attachment.id);
+
+              $().attr()
 
               // Hide the add image link
               addImgLink.addClass( 'hidden' );
