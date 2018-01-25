28f89b40edee96fdec44ce85a2e076a4c2ba8250
Nicola Carpene <zeronicola3@gmail.com>
2018-01-25 15:31:22 +0100

    [mac2]

--- functions.php
+++ functions.php
@@ -609,4 +609,51 @@ function projects_to_client() {
 add_action( 'p2p_init', 'projects_to_client' );
 
 include('vc_addon.php');
+
+
+
+
+
+
+
+
+//  *******************    CUSTOM FIELD BACKGROUND SIZE GALLERY
+
+/**
+ * Add background-size fields to media uploader
+ *
+ * @param $form_fields array, fields to include in attachment form
+ * @param $post object, attachment record in database
+ * @return $form_fields, modified form fields
+ */
+ 
+function be_attachment_field_credit( $form_fields, $post ) {
+    $form_fields['image-bg-size'] = array(
+        'label' => 'Background size',
+        'input' => 'checkbox',
+        'value' => get_post_meta( $post->ID, 'image-bg-size', true ),
+        'helps' => 'Select background-size value for single image in the gallery',
+    );
+    
+    return $form_fields;
+}
+add_filter( 'attachment_fields_to_edit', 'be_attachment_field_credit', 10, 2 );
+
+/**
+ * Save values of Photographer Name and URL in media uploader
+ *
+ * @param $post array, the post data for database
+ * @param $attachment array, attachment fields from $_POST form
+ * @return $post array, modified post data
+ */
+function be_attachment_field_credit_save( $post, $attachment ) {
+    if( isset( $attachment['image-bg-size'] ) )
+        update_post_meta( $post['ID'], 'be_photographer_name', $attachment['be-photographer-name'] );
+    
+    return $post;
+}
+add_filter( 'attachment_fields_to_save', 'be_attachment_field_credit_save', 10, 2
+
+
+
 ?>
