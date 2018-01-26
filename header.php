5c3eb3cbf20e2b1d4242011488ade48f7a9a6fc7
Nicola Carpene <zeronicola3@gmail.com>
2018-01-26 16:44:52 +0100

    [mac2]

--- header.php
+++ header.php
@@ -96,7 +96,7 @@
 
     #loader .slogan-container {
       position: absolute;
-      width: fit-content;
+      width: fil-content;
       left: 50%;
       top: 50%;
       transform: translateX(-50%) translateY(-50%);
