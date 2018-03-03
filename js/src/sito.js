55a9768c6babe8cf9ff0c9d89bbc1b625b40247e
Nicola Carpene <zeronicola3@gmail.com>
2018-03-03 18:32:20 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -76,7 +76,7 @@ $(document).ready(function() {
 		var header=$("header");
 
 		// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-		window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+		$(window).scroll(function(){ // or window.addEventListener("scroll"....
 			var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
 
 			if(st > 70){
