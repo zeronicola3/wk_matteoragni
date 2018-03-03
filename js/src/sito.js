a1664e6a051aa4735d10ddf3ba2822cd4539d643
Nicola Carpene <zeronicola3@gmail.com>
2018-03-03 17:33:13 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -142,15 +142,15 @@ $(document).ready(function() {
 				if(item_bottom >= container_bottom) {
 					$('.timeline-item').removeClass('fixed').css({bottom: "0px", top: "unset", right: "0px" });
 				} else if((st >= item_top - 200) && (sb <= container_bottom)){
-					$('.timeline-item').addClass('fixed').css({top: "200px", right: + item_right + "px" });
+					$('.timeline-item').addClass('fixed').css({top: "200px", right: + item_right + "px", bottom: "unset" });
 				}
 
 			} else {
 
 				if(st < container_top - 200) {
-					$('.timeline-item').removeClass('fixed').css({top: "0px", right: "0px" });
+					$('.timeline-item').removeClass('fixed').css({top: "0px", right: "0px", bottom: "unset" });
 				} else if((st <= item_top - 200)){
-					$('.timeline-item').addClass('fixed').css({top: "200px", right: + item_right + "px" });
+					$('.timeline-item').addClass('fixed').css({top: "200px", right: + item_right + "px", bottom: "unset"  });
 				}
 			}
 
