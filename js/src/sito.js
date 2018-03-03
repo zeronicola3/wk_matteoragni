333d755daa75616e8f5d5cc8f5e91fcdd832b920
Nicola Carpene <zeronicola3@gmail.com>
2018-03-03 18:01:18 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -124,7 +124,7 @@ $(document).ready(function() {
 		var container_bottom = container_top + $(".timeline-block").outerHeight();
 		
 
-		$(window).on("scroll", function(e){	
+		window.addEventListener("scroll", function(){	
 			var st = window.pageYOffset || document.documentElement.scrollTop;
 
 			var abs_top = item_top - container_top;
