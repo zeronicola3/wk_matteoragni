89fb77be4cf1c4392658fcb9c35a1bc04197ebe9
Nicola Carpene <zeronicola3@gmail.com>
2018-03-03 16:20:27 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -142,9 +142,9 @@ $(document).ready(function() {
 				console.log(st + " " + container_top);
 
 				if(st < container_top - 200) {
-					$('.timeline-item').removeClass('fixed');
+					$('.timeline-item').removeClass('fixed').css({ right: "0px" });
 				} else if(st >= item_top - 200){
-					$('.timeline-item').addClass('fixed').css({ right: + item_right*2 + "px" });
+					$('.timeline-item').addClass('fixed').css({ right: + item_right + "px" });
 				}
 			}
 
