0af1a39c6a792a2f7ec372fff37162719f197ae1
Nicola Carpene <zeronicola3@gmail.com>
2018-03-04 11:41:04 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -183,7 +183,7 @@ $(document).ready(function() {
 		$(this).parent().toggleClass('active');
 	});
 
-	$('.timeline-item-title').in('hover', function(){
+	$('.timeline-item-title h5').in('hover', function(){
 		var item = $(this).parent('a.timeline-title-box').attr('data-title');
 		$('.timeline-item.active').removeClass('active');
 		$('.timeline-item.' + item).addClass('active');
