f04dfd2029938e088cf008a099cc1bce1c115956
Nicola Carpene <zeronicola3@gmail.com>
2018-03-04 18:18:02 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -181,10 +181,10 @@ $(document).ready(function() {
 
 	$(window).scroll(function(){
 		var st = window.pageYOffset || document.documentElement.scrollTop;
-		
+
 		var nextItem = $(".timeline-item-title.active").last().next();
 
-		if(nextItem.offset().top <= (st - (screenheight / 2))){
+		if(nextItem.offset().top <= (st + (screenheight / 2))){
 
 		}
 
