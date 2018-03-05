29b8544cbff23c770ba66a545f29a4db4ef8a483
Nicola Carpene <zeronicola3@gmail.com>
2018-03-05 12:51:09 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -186,7 +186,7 @@ $(document).ready(function() {
 
 			var st = window.pageYOffset || document.documentElement.scrollTop;
 			var screenCenter = st + ((screenheight*2)/3);
-			var lastItem = $(".timeline-year-box.active").last();
+			var lastItem = $(".timeline-year-box.active:last-child").;
 			var nextItem = $(lastItem).next();
 			var curTop = $(nextItem).offset().top;
 
