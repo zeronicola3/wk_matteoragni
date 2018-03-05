8bd927df76118ccf1071237913671fe4de1157b7
Nicola Carpene <zeronicola3@gmail.com>
2018-03-05 12:49:08 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -193,7 +193,7 @@ $(document).ready(function() {
 			if(curTop <= screenCenter){
 				$(nextItem).addClass('active');
 			}
-			var lasttop = $(lastItem + ' .timeline-item-title').offset();
+			var lasttop = $(lastItem + ' a .timeline-item-title').offset();
 
 			if(lasttop.top == screenCenter){
 				var item = $(lastItem + ' .timeline-item-title').parent('a.timeline-title-box').attr('data-title');
