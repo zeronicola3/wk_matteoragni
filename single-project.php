7739f3154a6b52852cce1c65b123e81c023f01e6
Nicola Carpene <zeronicola3@gmail.com>
2018-03-22 18:35:44 +0100

    [mac2]

--- single-project.php
+++ single-project.php
@@ -5,6 +5,7 @@
 
     <div class="next-project">
         <?php $post = get_post($next_project->ID); ?>
+        <?php include get_template_directory() . '/assets/single-project-content.php'; ?>
     </div>
 
 
