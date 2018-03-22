c0180505d76206c3ba9a82bfdf3c60bb9f49fb8b
Nicola Carpene <zeronicola3@gmail.com>
2018-03-22 18:37:41 +0100

    [mac2]

--- single-project.php
+++ single-project.php
@@ -3,6 +3,51 @@
 		<?php include get_template_directory() . '/assets/single-project-content.php'; ?>
 	</div>
 
+     <?php 
+     $data = parse_json_file(); 
+    
+
+     function get_next_project($data, $year, $project) {
+        reset($data);
+
+        //$next_year = current($data);
+        $key_year = key($data);
+
+        while($key_year != $year){
+
+            $next_year = next($data);
+            $key_year = key($data);
+        }
+
+        $data_year = current($data);
+        
+        reset($data_year);
+
+        $key_project = key($data_year);
+
+        while($project != $key_project){
+            $next_project = next($data_year);
+            $key_project = key($data_year);
+        }
+
+
+        if(next($data_year) == null){
+            $next_year = next($data);
+            $data_year = current($data);
+            reset($data_year);
+            return current($data_year);
+        } else {
+            return current($data_year);
+        }
+     }
+
+
+
+    $next_project = get_next_project($data, $meta['webkolm_project_year']['0'], $post->post_name);
+
+     ?>
+
+    
     <div class="next-project">
         <?php $post = get_post($next_project->ID); ?>
         <?php include get_template_directory() . '/assets/single-project-content.php'; ?>
