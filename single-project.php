9f7bf688bb67139bbc796cb5fb328dd0ccd8c53c
Nicola Carpene <zeronicola3@gmail.com>
2018-03-22 21:05:54 +0100

    [mac2]

--- single-project.php
+++ single-project.php
@@ -4,17 +4,21 @@
         $post = get_queried_object();
 
         include get_template_directory() . '/assets/single-project-content.php'; ?>
+
+        <?php 
+        
+        $data = parse_json_file(); 
+        
+        $next_project = get_next_project($data, $meta['webkolm_project_year']['0'], $post->post_name);
+
+        wp_reset_postdata();
+
+        ?>
+
+        <div class="wrapper next-project-button">Next project: <a><?php echo $next_project['title']; ?></a></div>
 	</div>
 
-    <?php 
-    
-    $data = parse_json_file(); 
-    
-    $next_project = get_next_project($data, $meta['webkolm_project_year']['0'], $post->post_name);
 
-    wp_reset_postdata();
-
-     ?>
 
 
     <div class="next-project">
