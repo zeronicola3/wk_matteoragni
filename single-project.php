bbc83b792a7013917da25f2750b50d6ea1def5df
Nicola Carpene <zeronicola3@gmail.com>
2018-03-22 18:52:19 +0100

    [mac2]

--- single-project.php
+++ single-project.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 	<div id="contenuti" class="current-project">
-		<?php include get_template_directory() . '/assets/single-project-content.php'; ?>
+		<?php 
+        $post = get_queried_object();
+
+        include get_template_directory() . '/assets/single-project-content.php'; ?>
 	</div>
 
      <?php 
@@ -45,12 +48,15 @@
 
     $next_project = get_next_project($data, $meta['webkolm_project_year']['0'], $post->post_name);
 
+    wp_reset_postdata();
+
      ?>
 
 
     <div class="next-project">
-        <?php $post = get_post($next_project->ID); 
-            setup_postdata($next_project->ID);
+        <?php 
+        $post = get_post($next_project['ID']); 
+        setup_postdata( $post ); 
         ?>
         <?php include get_template_directory() . '/assets/single-project-content.php'; ?>
     </div>
