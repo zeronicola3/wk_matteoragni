6c305945ba0512e33552ac3325fe114caa0493b8
Nicola Carpene <zeronicola3@gmail.com>
2018-03-22 22:12:21 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -291,7 +291,8 @@ $(document).ready(function() {
 		event.preventDefault();
 		//.attr('id', 'contenuti').removeClass('next-project').addClass('current-project');
 		$('#contenuti').animate({ opacity: '0' }).slideToggle();
-		$('.next-project').css('height', 'auto').scrollTo(0,0);
+		window.scrollTo(0,0);
+		$('.next-project').css('height', 'auto');
 		
 		//var next_button = 'include get_template_directory() . "/assets/next-project-button.php"; ?>';
 		//var next_project = '<?php include get_template_directory() . "/assets/next-project-block.php"; ?>';
