a82adc0c5bfb2d35b0985e6c6566774c2a622005
Nicola Carpene <zeronicola3@gmail.com>
2018-03-22 23:18:08 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -314,16 +314,17 @@ $(document).ready(function() {
 		//$('#contenuti').append(next_button);
 		//$('#contenuti').after(next_project)
 	});
-function resetScrollPos(selector) {
-  var divs = document.querySelectorAll(selector);
-  for (var p = 0; p < divs.length; p++) {
-    if (Boolean(divs[p].style.transform)) { //for IE(10) and firefox
-      divs[p].style.transform = 'translate3d(0px, 0px, 0px)';
-    } else { //for chrome and safari
-      divs[p].style['-webkit-transform'] = 'translate3d(0px, 0px, 0px)';
-    }
-  }
-}
+	
+	function resetScrollPos(selector) {
+	  var divs = document.querySelectorAll(selector);
+	  for (var p = 0; p < divs.length; p++) {
+	    if (Boolean(divs[p].style.transform)) { //for IE(10) and firefox
+	      divs[p].style.transform = 'translate3d(0px, 0px, 0px)';
+	    } else { //for chrome and safari
+	      divs[p].style['-webkit-transform'] = 'translate3d(0px, 0px, 0px)';
+	    }
+	  }
+	}
 	
 	/* CAROUSEL POST
 
