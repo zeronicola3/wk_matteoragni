c46349ce440b68705de1193cbce49c57a364c4e0
Nicola Carpene <zeronicola3@gmail.com>
2018-03-22 23:13:31 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -295,6 +295,7 @@ $(document).ready(function() {
 		//$('#contenuti').animate({ opacity: '0' }).slideToggle();
 
 		$('#contenuti').animate({ opacity: '0' }).slideToggle();
+		
 		var timeout = setTimeout(function(){
 			$('.next-project').addClass('active');
 			resetScrollPos('.next-project');
