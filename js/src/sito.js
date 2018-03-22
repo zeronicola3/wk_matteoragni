d54c87e8262178d73af3ef375007bc6572939bf2
Nicola Carpene <zeronicola3@gmail.com>
2018-03-22 22:43:35 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -293,10 +293,11 @@ $(document).ready(function() {
 
 		//.attr('id', 'contenuti').removeClass('next-project').addClass('current-project');
 		//$('#contenuti').animate({ opacity: '0' }).slideToggle();
+		$('.next-project').addClass('active');
 		
-		$('#contenuti').animate({ opacity: '0' }).slideToggle();
 		var timeout = setTimeout(function(){
-			$('.next-project').addClass('active');
+			$('#contenuti').animate({ opacity: '0' }).slideToggle();
+			
 		}, 1000);
 
 		
