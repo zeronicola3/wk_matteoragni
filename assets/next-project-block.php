4bf4a48e0bf1d102faa1d9c345d60ae14c4962f1
Nicola Carpene <zeronicola3@gmail.com>
2018-03-22 21:40:15 +0100

    [mac2]

---
+++ assets/next-project-block.php
@@ -0,0 +1,7 @@
+<div class="next-project">
+    <?php 
+    $post = get_post($next_project['ID']); 
+    setup_postdata( $post ); 
+    ?>
+    <?php include get_template_directory() . '/assets/single-project-content.php'; ?>
+</div>
--- single-project.php
+++ single-project.php
@@ -9,16 +9,7 @@
 
 	</div>
 
-
-
-
-    <div class="next-project">
-        <?php 
-        $post = get_post($next_project['ID']); 
-        setup_postdata( $post ); 
-        ?>
-        <?php include get_template_directory() . '/assets/single-project-content.php'; ?>
-    </div>
+    <?php include get_template_directory() . '/assets/next-project-block.php'; ?>
 
 
 <?php get_footer(); ?>
