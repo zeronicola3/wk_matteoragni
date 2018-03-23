4ccbe4afadfa8d993d2789e8df41015fee38fab8
Nicola Carpene <zeronicola3@gmail.com>
2018-03-23 14:14:32 +0100

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -344,7 +344,7 @@ $(document).ready(function() {
 			    // SE LA RICERCA VA A BUON FINE
 			    } else {			    	
 			    	console.log('click');
-			    	$('.current-project').after(result);
+			    	//$('.current-project').after(result);
 			    }
 		    }
 	    });
