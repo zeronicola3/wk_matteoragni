f6d9442b76c90ac36be5ac5cba47645049abcf7c
Nicola Carpene <zeronicola3@gmail.com>
2018-03-23 17:16:26 +0100

    [mac2]

--- assets/single-project-content.php
+++ assets/single-project-content.php
@@ -84,3 +84,23 @@
     
 
 </div>
+
+<?php 
+        
+$connected = p2p_type( 'projects_to_projects' )->set_direction( 'to' )->get_connected( $post->ID );
+
+
+print_r($connected);
+
+$number = 0;
+
+foreach ($connected as $conn) {
+
+    print_r($conn);
+
+}
+
+
+
+
+?>
