4e368e582252b19922096036edbc2f2a07fb9d84
Nicola Carpene <zeronicola3@gmail.com>
2018-03-23 17:07:59 +0100

    [mac2]

--- assets/next-project-button.php
+++ assets/next-project-button.php
@@ -1,6 +1,6 @@
 <?php 
         
-$connected = p2p_type( 'projects_to_client' )->set_direction( 'to' )->get_connected( $post->ID );
+$connected = p2p_type( 'projects_to_projects' )->set_direction( 'to' )->get_connected( $post->ID );
 
 
 print_r($connected);
