351dece8fc65b8f78f870a89b32168b225aa4a09
Nicola Carpene <zeronicola3@gmail.com>
2018-03-23 17:25:20 +0100

    [mac2]

--- assets/single-project-content.php
+++ assets/single-project-content.php
@@ -87,7 +87,7 @@
 </div>
 
 <?php 
-    
+    echo $post->ID;
     $connected1 = p2p_type( 'projects_to_projects')->set_direction( 'to' )->get_connected( $post->ID );
 
      foreach ($connected1 as $conn1) {
