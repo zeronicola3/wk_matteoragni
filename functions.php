c44c21145a86dde71f0f3655815cde072ce48532
Nicola Carpene <zeronicola3@gmail.com>
2018-03-24 16:06:35 +0100

    [mac2]

--- functions.php
+++ functions.php
@@ -934,9 +934,9 @@ function webkolm_ajax_next_project() {
         // PRENDO LA STRINGA DA CERCARE
     $project_id = sanitize_text_field( $_POST[ 'project' ] );
 
-    echo $project_id;
+    return $project_id;
 
-    $connected1 = p2p_type( 'projects_to_projects')->set_direction( 'to' )->get_connected( $project_id );
+    //$connected1 = p2p_type( 'projects_to_projects')->set_direction( 'to' )->get_connected( $project_id );
 
 
 
