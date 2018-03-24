bae3ca640d9c30f9a3fd1b7a512a661b3c85d4b7
Nicola Carpene <zeronicola3@gmail.com>
2018-03-24 21:48:19 +0100

    [mac2]

--- assets/timeline_functions.php
+++ assets/timeline_functions.php
@@ -87,7 +87,7 @@ function get_project_type($post_id) {
 
 	$term = wp_get_post_terms( $post_id, 'project_type', array('fields' => 'all') );
 
-	echo $term[0]->term_slug;
+	echo $term[0]->slug;
 }
 
 
