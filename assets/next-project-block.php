2067167bf5b67a6d4243d62ad9f8e5b7ada38a32
Nicola Carpene <zeronicola3@gmail.com>
2018-03-24 16:39:21 +0100

    [mac2]

--- assets/next-project-block.php
+++ assets/next-project-block.php
@@ -1,4 +1,4 @@
 
 <div class="next-project" data-id="<?php echo $post->ID; ?>">
-    <?php include get_template_directory() . '/assets/single-project-content.php'; ?>
+    <?php get_template_part( get_template_directory() . '/assets/single-project-content'); ?>
 </div>
