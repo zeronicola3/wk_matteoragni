d94dcfca5402d696456a6ae9276d92a6a71d51b6
Nicola Carpene <zeronicola3@gmail.com>
2018-03-25 20:05:38 +0200

    [mac2]

--- js/src/sito.js
+++ js/src/sito.js
@@ -315,6 +315,7 @@ $(document).ready(function() {
 
 
 	$('.next-project-button a').on('click', function(event){
+		
 		event.preventDefault();
 
 		//.attr('id', 'contenuti').removeClass('next-project').addClass('current-project');
