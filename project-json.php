1277b8c6b9bc4796e203613482ad3b13a89bb9f6
Nicola Carpene <zeronicola3@gmail.com>
2018-03-25 10:02:45 +0200

    [mac2]

--- project-json.php
+++ project-json.php
@@ -42,8 +42,13 @@ function primary_project_content($project) { ?>
 		</div>
 		<div class="timeline-item-description">
 			<h4 class="project-title"><?php echo $project['title']; ?></h4>
+			<?php if($project['description'] != ""){ ?>
 			<span class="project-client"><?php echo $project['description']; ?></span>
-			<span class="project-client"><?php echo $project['clienti'][0]['title']; ?></span>
+			<?php 
+			}
+			if($project['clienti'][0]['title'] != ""){ ?>
+			<span class="project-client"><?php echo $project['clienti'][0]['title']; ?></span><br>
+			<?php } ?>
 			<span class="project-client"><?php echo $project['year']; ?></span>
 
 		</div>
