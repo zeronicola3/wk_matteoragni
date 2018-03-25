c1adcd360596b9b3e3e2801eb46baf0f0e52ce7b
Nicola Carpene <zeronicola3@gmail.com>
2018-03-25 20:55:22 +0200

    [mac2]

--- assets/single-project-content.php
+++ assets/single-project-content.php
@@ -105,7 +105,7 @@
         $post = get_post($conn1->ID);
         setup_postdata($post); ?>
 
-        <div class="wrapper next-project-button">Related project: <a href=""><?php echo $post->post_title; ?></a></div>
+        <div class="wrapper next-project-button">Related project: <a><?php echo $post->post_title; ?></a></div>
 
     <?php } ?>
 
