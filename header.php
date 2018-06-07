afa3104b1272b219f78507b7e310e226a19e9067
Nicola Carpene <zeronicola3@gmail.com>
2018-06-07 09:42:29 +0200

    [mac2]

--- header.php
+++ header.php
@@ -73,7 +73,7 @@
     // progressbar.js@1.0.0 version is used
     // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
-    var bar = new ProgressBar.Line(progressbar, {
+    var bar = new ProgressBar.Line('#progressbar', {
       strokeWidth: 3,
       easing: 'easeInOut',
       duration: 1400,
