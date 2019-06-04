b168a1f35ba3dc7a55bd74f5a88f32db333c6fed
Nicola Carpene <zeronicola3@gmail.com>
2019-06-04 10:07:09 +0200

    [mac2]

--- block_menuwide.php
+++ block_menuwide.php
@@ -9,7 +9,15 @@
         ); 
 ?>
 	<ul class="social-nav nomobile">
-		<li class="search-btn"><a href=""></a></li>
+		<li class="search-btn"><a href="">
+			<svg version="1.1" id="Livello_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 49.9 595.3 594.3" enable-background="new 0 49.9 595.3 594.3" xml:space="preserve">
+<path d="M591.3,580.1L405.1,393.9c24.2-36.1,37.2-78.5,37.2-122.8c0-59.1-23-114.6-64.8-156.4c-41.8-41.8-97.3-64.8-156.4-64.8
+	s-114.6,23-156.4,64.8C23,156.5,0,212,0,271.1s23,114.6,64.8,156.4s97.3,64.8,156.4,64.8c44.9,0,87.7-13.3,124-38l186.1,186
+	c6.3,6.3,17.8,4.9,25.8-3l31.3-31.3C596.3,598,597.6,586.4,591.3,580.1z M221.1,432.3c-88.9,0-161.2-72.3-161.2-161.2
+	s72.3-161.2,161.2-161.2s161.2,72.3,161.2,161.2S310,432.3,221.1,432.3z"/>
+</svg>
+		</a></li>
 		<li class="social-btn fb-btn"><a href="" ></a></li>
 		<li class="social-btn fb-btn"><a href="" class="social-btn fb-btn"></a></li>
 	</ul>
