4016a8827740341b9ad350e86b635c87dafc998c
Nicola Carpene <zeronicola3@gmail.com>
2019-06-10 10:29:57 +0200

    [mac2]

--- index.php
+++ index.php
@@ -210,7 +210,7 @@ get_header(); ?>
                 'ignore_custom_sort' => true,
                 'orderby' => 'menu_order',
                 'order' => 'ASC',
-                'post_:not_in' => $selected_posts
+                'post__not_in' => $selected_posts
             );
 
             $query = new WP_Query($args);
@@ -222,36 +222,39 @@ get_header(); ?>
                 while ( $query->have_posts() ) : $query->the_post(); 
 
                     $image_id = get_post_meta(get_the_ID(), 'webkolm_featured_img_input', true);
-                    $url_small = wp_get_attachment_image_src( $image_id, 'medium' );
-                    $url_big = wp_get_attachment_image_src( $image_id, 'large' );
 
-                    // SE immagine scura, allora frecce bianche
-                    $is_dark_image = get_post_meta(get_the_ID(), 'webkolm_dark_image', true);
-                    $cursor_color = 'black';
-                    if($is_dark_image) {
-                        $cursor_color = 'white';
+                    if($image_id != ""){
+                        $url_small = wp_get_attachment_image_src( $image_id, 'medium' );
+                        $url_big = wp_get_attachment_image_src( $image_id, 'large' );
+
+                        // SE immagine scura, allora frecce bianche
+                        $is_dark_image = get_post_meta(get_the_ID(), 'webkolm_dark_image', true);
+                        $cursor_color = 'black';
+                        if($is_dark_image) {
+                            $cursor_color = 'white';
+                        }
+
+                        $connected = p2p_type( 'projects_to_client' )->set_direction( 'to' )->get_connected( get_the_ID() );
+
+                        foreach ($connected as $conn) {
+                            if($conn->post_title != get_the_title())
+                                $client_name = " - " . $conn->post_title;
+                            else
+                                $client_name = "";
+                        }
+                        
+
+                        ?>
+
+                        <div class="grid-item">
+                            <a href="<?php echo get_the_permalink(); ?>" class="tile-content" style="background-image: url('<?php echo $url_small[0]; ?>');">
+                                <span class="tile-title" style="color:<?php echo $cursor_color; ?>;"><?php the_title(); ?><?php echo $client_name; ?></span>
+                            </a>
+                        </div>
+
+                        <?php
+                        $numslide++;
                     }
-
-                    $connected = p2p_type( 'projects_to_client' )->set_direction( 'to' )->get_connected( get_the_ID() );
-
-                    foreach ($connected as $conn) {
-                        if($conn->post_title != get_the_title())
-                            $client_name = " - " . $conn->post_title;
-                        else
-                            $client_name = "";
-                    }
-                    
-
-                    ?>
-
-                    <div class="grid-item">
-                        <a href="<?php echo get_the_permalink(); ?>" class="tile-content" style="background-image: url('<?php echo $url_small[0]; ?>');">
-                            <span class="tile-title" style="color:<?php echo $cursor_color; ?>;"><?php the_title(); ?><?php echo $client_name; ?></span>
-                        </a>
-                    </div>
-
-                    <?php
-                    $numslide++;
                 endwhile;
             }
 
